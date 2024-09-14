b55ab69ef7eb05ce93771e845c4349625bdb7a85
Harshit Kumar <15359962+harshitkumar31@users.noreply.github.com>
2024-09-14 15:59:42 +0530

    feat: gridmaker for artists

---
+++ index.py
@@ -0,0 +1,43 @@
+import matplotlib.pyplot as plt
+import matplotlib.ticker as plticker
+try:
+    from PIL import Image
+except ImportError:
+    import Image
+
+# Open image file
+image = Image.open('Ganesh_4x4_1.jpeg')
+my_dpi=100.
+
+# Set up figure
+fig=plt.figure(figsize=(float(image.size[0])/my_dpi,float(image.size[1])/my_dpi),dpi=my_dpi)
+ax=fig.add_subplot(111)
+
+# Remove whitespace from around the image
+fig.subplots_adjust(left=0,right=1,bottom=0,top=1)
+
+# Set the gridding interval: here we use the major tick interval
+myInterval=100.
+loc = plticker.MultipleLocator(base=myInterval)
+ax.xaxis.set_major_locator(loc)
+ax.yaxis.set_major_locator(loc)
+
+# Add the grid
+ax.grid(which='major', axis='both', linestyle='-', color='white')
+
+# Add the image
+ax.imshow(image)
+
+# Find number of gridsquares in x and y direction
+nx=abs(int(float(ax.get_xlim()[1]-ax.get_xlim()[0])/float(myInterval)))
+ny=abs(int(float(ax.get_ylim()[1]-ax.get_ylim()[0])/float(myInterval)))
+
+# Add some labels to the gridsquares
+for j in range(ny):
+    y=myInterval/2+j*myInterval
+    for i in range(nx):
+        x=myInterval/2.+float(i)*myInterval
+        ax.text(x,y,'{:d}'.format(i+j*nx + 1),color='white',ha='center',va='center')
+
+# Save the figure
+fig.savefig('Ganesh_4x4_1_grid.jpeg',dpi=my_dpi)
